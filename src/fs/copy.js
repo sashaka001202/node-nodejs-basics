ac82f78bec64d5fb71db39e37afee7f5662aaecf
alex <sashaka00120@mail.ru>
2022-06-01 18:46:50 +0300

    feat: complete task fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,31 @@
+import {constants} from 'fs'
+import {readdir, mkdir, copyFile} from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 export const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const oldPath = path.join(__dirname, 'files')
+    const copyPath = path.join(__dirname, 'files_copy')
+
+    try {
+
+        const files = await readdir(oldPath, { withFileTypes: true });
+        await mkdir(copyPath, { recursive: true });
+
+        for (const file of files) {
+            const srcName = path.join(oldPath, file.name);
+            const destName = path.join(copyPath, file.name);
+            await copyFile(srcName, destName,constants.COPYFILE_EXCL);
+
+        }
+
+
+    } catch (error) {
+        console.error('FS operation failed');
+    }
+
 };
+
+copy()
