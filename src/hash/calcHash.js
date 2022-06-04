b86538ff8ecc63dd0eb172edd58a1b862fcb0557
alex <sashaka00120@mail.ru>
2022-06-04 13:59:25 +0300

    feat: complete task hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,29 @@
+import { readFile } from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { createHash } from 'crypto'
+
+
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const resultPath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
+    try {
+
+        const data = await readFile(resultPath, 'utf8');
+        console.log(data);
+        const hash = createHash('sha256')
+            .update(data).digest("hex");
+        console.log(hash);
+
+    } catch (error) {
+        console.error('FS operation failed');
+    }
+
 };
+
+calculateHash()
+
+
