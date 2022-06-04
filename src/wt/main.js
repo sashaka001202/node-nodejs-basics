06d159ec447a12168f6e5ce0f394ac702b90a932
alex <sashaka00120@mail.ru>
2022-06-05 00:27:34 +0300

    feat: complete task workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,42 @@
+import { Worker } from 'worker_threads';
+import os from 'os'
+import path from  'path'
+import { fileURLToPath } from 'url'
+
 export const performCalculations = async () => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const resultPath = path.join(__dirname,'worker.js')
+    const numberCPU = os.cpus().length
+    let workersResult = []
+
+    const runService = (workerData) => {
+        return new Promise((resolve) => {
+
+            const worker = new Worker(resultPath, { workerData });
+            worker.on('message', (data) => {
+                resolve({
+                    status: 'resolved',
+                    data: data,
+                });
+            })
+            worker.on('error', (e) => {
+                resolve({
+                    status: 'error',
+                });
+            })
+        })
+    }
+
+    const run = async () => {
+        for (let index = 0; index < numberCPU; index++) {
+            const result = await runService(index + 10)
+            workersResult.push(result)
+        }
+        console.log(workersResult);
+    }
+    run()
+
 };
+
+performCalculations()
